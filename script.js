6b8ccaaea841b7937c488ab6e8600e5a7c072271
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-23 22:00:52 +0100

    add-login-one

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Helllo DOM");
+document.getElementById("btn-login").addEventListener('click' , function(event){
+    event.preventDefault();
+    //console.log("LogIn Button Clicked");
+    const phoneNumber = document.getElementById("phone-number").value  //sathe value dile shodu matro value ta dhekabe;
+
+    const pinNumber = document.getElementById("pin-number").value
+    console.log(phoneNumber , pinNumber);
+    
+    // This is Temoporary,You Should not use it
+    if(phoneNumber === "01781233421" && pinNumber === "1234") {
+        console.log("You Are Loged In");
+    } else {
+        alert("Wrong Phone Number Or Pin")
+    }
+})
