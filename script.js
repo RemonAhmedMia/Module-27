a0e8a068586d1bc54155a749cecfd42422556e09
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-23 23:00:38 +0100

    first-add-money

--- script.js
+++ script.js
@@ -14,4 +14,31 @@ document.getElementById("btn-login").addEventListener('click' , function(event){
     } else {
         alert("Wrong Phone Number Or Pin")
     }
-})
+});
+
+// Practice-01
+
+// document.getElementById("btn-login").addEventListener('click' , function(event){
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById("phone-number").value
+//     const pinNumber = document.getElementById("pin-number").value
+//     if (phoneNumber === "01318664312" && pinNumber === "1234") {
+//         window.location.href = "/home.html"
+//     } else {
+//         alert("Wrong Phone Number Or Pin");
+//     }
+// })
+
+// **Practice -02
+
+// document.getElementById("btn-login").addEventListener('click' , function(event){
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById("phone-number").value
+//     const pinNumber = document.getElementById("pin-number").value
+
+//     if (phoneNumber === "01318664312" && pinNumber === "1234") {
+//         window.location.href = "/home.html"
+//     } else {
+//         alert("Wrong number or pin")
+//     }
+// })
