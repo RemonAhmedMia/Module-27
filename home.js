06873ffe0aac8973720db3e9a3588117d6fa5f2e
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-23 23:20:19 +0100

    add-money-eventhandler-one

---
+++ home.js
@@ -0,0 +1,14 @@
+//Add Money To The Account;
+
+document.getElementById("btn-add-money").addEventListener('click' , function(event) {
+    event.preventDefault();
+   // console.log("Money Dao Amake");
+    
+    // Get Money Addedd To the Accont
+    const addMoneyInput = document.getElementById("add-money-input").value
+    // console.log(addMoneyInput);
+    
+    // Get The Pin Number:
+    const inputPinNumber = document.getElementById("input-pin-number").value
+    
+})
